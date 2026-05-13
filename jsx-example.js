8d1890c000b1526a2cae5ea34c52374ab1728438
Eda Yavuz <edayavuzcontact@gmail.com>
2026-05-13 23:47:43 +0300

    Add a simple JSX example component

---
+++ jsx-example.js
@@ -0,0 +1,12 @@
+function App() {
+  const name = "Eda";
+
+  return (
+    <div>
+      <h1>Merhaba {name}</h1>
+      <p>Bu basit bir JSX örneğidir.</p>
+    </div>
+  );
+}
+
+export default App;
