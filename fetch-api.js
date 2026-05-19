356167023f6063cc6233a822cc9b7bb727feea57
Eda Yavuz <edayavuzcontact@gmail.com>
2026-05-19 23:08:02 +0300

    Add fetch API to retrieve and display users

---
+++ fetch-api.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => (
+        <h2 key={user.id}>{user.name}</h2>
+      ))}
+    </div>
+  );
+}
+
+export default App;
