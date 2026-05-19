6371af66e0bb7377d02160f1e8762f31b77f847e
Eda Yavuz <edayavuzcontact@gmail.com>
2026-05-19 23:08:41 +0300

    Add axios API call to fetch users

---
+++ axios-api.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => (
+        <h2 key={user.id}>{user.name}</h2>
+      ))}
+    </div>
+  );
+}
+
+export default App;
